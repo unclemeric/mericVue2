984426520d37bf2285e772c1dce5d3bad5aafac0
laisf <sanfa.lai@partner.midea.com.cn>
2017-02-10 10:37:23 +0800

    代码初始化   提交

---
+++ src/entry/App.vue
@@ -0,0 +1,83 @@
+<template>
+	<div>
+		<el-button @click="show = !show">
+			<span>{{!show ? '显示' :'关闭'}}</span>
+		</el-button>
+		
+		<transition name="fade" appear>
+			<div v-show="show" class="apps">
+				<ul>
+					<li v-for="item in message">
+						{{item}}
+					</li>
+				</ul>
+				hello vue2!!
+				{{message}}
+				<br/>
+				<el-button @click="showmsg=!showmsg">click me <span v-if="showmsg">to close</span></el-button>
+				<transition name="fade" appear>
+					<span v-show="showmsg" class="appmsg">
+						你真的点啊？~~🐶
+					</span>
+				</transition>
+			</div>
+		</transition>
+	</div>
+</template>
+<script>
+	import Vue from 'vue';
+	import { Button } from 'element-ui';
+	Vue.component(Button.name, Button)
+	export default {
+		data(){
+			return {
+				show:true,
+				showmsg:false,
+				message:'hello world!'
+			}
+		}
+	}
+</script>
+<style scoped>
+.apps {
+	font-size: 30px;
+	position: relative;
+	transition: all 1s;
+	left:300px;
+  	font-size: 20px;
+}
+.appmsg {
+	font-size: 30px;
+	position: relative;
+	transition: all 1s;
+	left:300px;
+  	font-size: 20px;
+}
+/!**
+ * 正在进入状态
+ */
+.fade-enter-active {
+	color:#eee; 
+}
+/**
+ * 初始状态
+ */
+.fade-enter{
+  left:0px;
+  font-size: 9px;
+}
+/**
+ * 离开状态
+ */
+.fade-leave{
+	font-size: 700
+}
+/**
+ * 正在离开状态
+ */
+ .fade-leave-active{
+ 	left: 0;
+	opacity: 0;
+}
+	
+</style>
---
+++ webpack.config.pro.js
@@ -0,0 +1,116 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var glob = require('glob');
+var path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+var nodeModules = resolve('node_modules');
+var port = 8080;
+var rootPath = resolve('./dist');
+
+var _entries = {};
+var fileNames=[];
+var jsDir = resolve('./src/entry');
+var entryFiles = glob.sync(`${jsDir}/*.js`);
+entryFiles.forEach((filePath) => {
+    var filename = filePath.substring(filePath.lastIndexOf('/')+1,filePath.lastIndexOf('.'));
+    _entries[filename] = filePath;
+    fileNames.push(filename);
+});
+
+var webpackConfig = {
+  entry: Object.assign({},{"vendor":['vue']},_entries),
+  output: {
+    filename: '[name].[hash:8].min.js',
+    path: rootPath,
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      'vue$': 'vue/dist/vue.common.js',
+    }
+  },
+  devServer: {
+    port: port
+  },
+  devtool: '#eval-source-map',
+  module: {
+    rules: [{
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }, {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: nodeModules,
+      }, {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({ fallbackLoader: 'vue-style-loader', loader: 'css-loader' }),
+      }, {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({ fallbackLoader: 'vue-style-loader', loader: 'css-loader!sass-loader?indentedSyntax'}),
+      },
+      // loader png or jpg or git and svg files 然后压缩之，并把小于10kb的图片base64格式内联到css文件中。
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'image-webpack-loader?{progressive:true, optimizationLevel: 4, ' +
+          'interlaced: false, pngquant:{quality: "65-90", speed: 4}}', // 压缩图片
+          'url-loader?limit=10000&name=img/[name].[hash:8].[ext]', // 小于10kb的图片base64格式内联到css文件中。
+        ],
+      }, {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader?limit=10000&name=fonts/[name].[hash:8].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': "'production'"
+    }),
+    new ExtractTextPlugin({filename:'css/[name].[hash:8].min.css',disable: false, allChunks: true}),//可以将所有css文件打包到css/main.css文件中
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.ProvidePlugin({'Vue': "vue"}),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+    }),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new webpack.LoaderOptionsPlugin({
+      // test: /\.xxx$/, // may apply this only for some modules
+      options: {
+        vue: {
+          loaders: {
+            css: ExtractTextPlugin.extract({ fallbackLoader: 'vue-style-loader', loader: 'css-loader' }),
+            postcss: ExtractTextPlugin.extract({ fallbackLoader: 'vue-style-loader', loader: 'css-loader' }),
+            sass: ExtractTextPlugin.extract({ fallbackLoader: 'vue-style-loader', loader: 'css-loader!sass-loader?indentedSyntax' })
+          }
+        }
+      }
+    })
+  ]
+}
+
+let htmlPageWebpack = fileNames.map((name)=>{
+  return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: 'index.html',
+      inject: true,
+      title: "hello page",
+      chunks:['vendor', name],
+      hash:true,
+      minify: {
+          removeComments: true,//移除注释
+          collapseWhitespace: true
+      }
+  });
+});
+
+webpackConfig.plugins = [].concat(webpackConfig.plugins,htmlPageWebpack);
+module.exports = webpackConfig;
